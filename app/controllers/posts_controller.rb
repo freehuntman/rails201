73e32f7a373cf4a9f7805d1f34d136e7b3e2ffbc
吴海铭 <freehuntman@gmail.com>
2017-03-30 07:55:25 +0800

    implement extra task&there is sth wrong with post show action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,56 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_filter :authenticate_user!, only: [:new, :create]
+
+  def show
+    @group = Group.fidn(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.user = current_user
+  end
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.user = current_user
+  end
+
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
-
     if @post.save
       redirect_to group_path(@group)
     else
       render :new
     end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.user = current_user
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.user = current_user
+    @post.destroy
+
+    redirect_to account_posts_path, alert: 'Post deleted'
   end
 
 
